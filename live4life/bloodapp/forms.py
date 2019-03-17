9b2b03f0cb36401a0ce6c234ac2761c33fe4e980
Ivan Saji Abraham <ivansajimeleth@gmail.com>
2019-03-17 21:36:40 +0530

    Error In forms
    Unable to setup choice field

---
+++ live4life/bloodapp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.utils import timezone
+
+from .models import Acceptor,Donor
+
+bloodgroup=('A +ve','A -ve','B +ve','B -ve',)
+
+class form1(forms.Form):
+    name = forms.CharField(label='name',max_length=50,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Name','id':'name',}))
+    group = forms.ChoiceField(choices=bloodgroup,label='group',widget=forms.Select)
+    location = forms.CharField(label='location',max_length=100,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Location','id':'location',}))
+    contact = forms.IntegerField(label='contact',widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Contact Number','id':'location','type':'number',}))
+
